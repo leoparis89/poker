314ea190e26592df592fe5ca80bebc8af9d02b7d
Lev Kowalski <levkowalski@gmail.com>
2020-04-28 17:54:46 +0200

    add express

--- back/index.ts
+++ back/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import * as express from "express";
+
+const app = express();
+
+const root = "/api";
+
+app.get(root + "/new", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("app started");
+});
