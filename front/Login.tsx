98b50a9fd1624635454c5d2aad4f05818bb531f2
Lev Kowalski <levkowalski@gmail.com>
2020-08-13 17:15:10 +0200

    google sign in logo

--- front/Login.tsx
+++ front/Login.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
+const googleLogin = require("./assets/googleSignIn/btn_google_signin_light_normal_web@2x.png");
+
 export function Login() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>React Poker</h1>
-      <Button href="http://localhost:3000/auth/google">Login</Button>
+
+      <a href="http://localhost:3000/auth/google">
+        <img src={googleLogin} />
+      </a>
     </div>
   );
 }
