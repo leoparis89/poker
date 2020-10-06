d97f8507f3a6ed521d08d1974db64e0ecd7a0e99
Lev Kowalski <levkowalski@gmail.com>
2020-10-06 12:31:04 +0200

    Change login image

--- front/Login.tsx
+++ front/Login.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const googleLogin = require("./assets/googleSignIn/btn_google_signin_light_normal_web@2x.png");
 
-const url = require("./assets/login-image.jpg");
+// const url = require("./assets/login-image.jpg");
 export function Login() {
   return (
     <div
       style={{
         textAlign: "center",
         // backgroundImage: `url("${url}")`,
+        backgroundPosition: "cover",
+        backgroundRepeat: "no-repeat",
         height: "100vh"
       }}
     >
