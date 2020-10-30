f5196b3dce57a4b296faad35fc5dc383bee26069
Lev Kowalski <levkowalski@gmail.com>
2020-10-31 00:03:39 +0100

    add css in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+require("cardsJS/dist/cards.js");
+require("cardsJS/dist/cards.css");
+import "../front/disableBootstrapCardStyle.css";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" }
 };
