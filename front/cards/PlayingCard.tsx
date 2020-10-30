889883316706a11f417217632307cf9b999f79c8
Lev Kowalski <levkowalski@gmail.com>
2020-10-30 16:00:14 +0100

    Reset bootstrap card style

--- front/cards/PlayingCard.tsx
+++ front/cards/PlayingCard.tsx
@@ -1,7 +1,5 @@
 import React, { FunctionComponent } from "react";
 import { RankSuit } from "../../common/models";
-require("cardsJS/dist/cards.js");
-require("cardsJS/dist/cards.css");
 
 export interface PlayingCardProps {
   rankSuit: RankSuit;
@@ -10,10 +8,6 @@ export const PlayingCard: FunctionComponent<PlayingCardProps> = ({
   rankSuit
 }) => {
   return (
-    <img
-      style={{ display: "inline" }}
-      className="card"
-      src={require(`cardsJS/cards/${rankSuit}.svg`)}
-    />
+    <img className="card" src={require(`cardsJS/cards/${rankSuit}.svg`)} />
   );
 };
--- front/index.tsx
+++ front/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+require("./disableBootstrapCardStyle.css");
+require("cardsJS/dist/cards.js");
+require("cardsJS/dist/cards.css");
 import "react-toastify/dist/ReactToastify.css";
 import ReactDOM from "react-dom";
 import { App } from "./App";
