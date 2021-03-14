ada85451df03f1739d63437d9759c920759cfefb
Lev Kowalski <levkowalski@gmail.com>
2021-03-14 14:30:25 +0100

    wip

--- front/index.tsx
+++ front/index.tsx
@@ -7,4 +7,4 @@ import "react-toastify/dist/ReactToastify.css";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-ReactDOM.render(<App></App>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
